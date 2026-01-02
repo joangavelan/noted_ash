df2dc3750f7fe50d114540340d8119596ee642f2
Egbert Pot <egbert@egbertpot.nl>
2026-01-02 16:45:54 +0100

    Use port 5481 for postgres

--- config/test.exs
+++ config/test.exs
@@ -11,6 +11,7 @@ config :noted, Noted.Repo,
   username: "postgres",
   password: "postgres",
   hostname: "localhost",
+  port: 5481,
   database: "noted_test#{System.get_env("MIX_TEST_PARTITION")}",
   pool: Ecto.Adapters.SQL.Sandbox,
   pool_size: System.schedulers_online() * 2
